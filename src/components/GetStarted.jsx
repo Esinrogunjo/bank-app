4766a29395b20b154af8182135dd38952cb167a4
Dauda Adbulganiyu <daudakale@gmail.com>
2022-08-16 13:59:41 +0100

    done with get started animation

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import styles from "../style";
 
 const GetStarted = () => (
   <div
-    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    className={`${styles.flexCenter} roundBox w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
   >
     <div
       className={` ${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}
